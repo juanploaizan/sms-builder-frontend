42d82c34ff5bf9cfc861bcb82c8ac530d374251d
Mateo Estrada Ramírez <mateoestradaramirez@outlook.com>
2024-11-21 00:25:12 +0000

    Comienza la implementación de importar estudios desde BD

---
+++ src/app/dashboard/import-from-db/import-from-db.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-import-from-db',
+  standalone: true,
+  imports: [],
+  templateUrl: './import-from-db.component.html',
+  styleUrl: './import-from-db.component.scss'
+})
+export class ImportFromDbComponent {
+
+}
